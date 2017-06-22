24629678fe2ca58d86dbc98d357518d0841acb5c
Min Borin <minborin.gitlab@gmail.com>
2017-06-22 12:03:34 +0700

    add first project

---
+++ src/main/java/com/hrd/borin/B012MinBorinMiniProjectApplication.java
@@ -0,0 +1,12 @@
+package com.hrd.borin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B012MinBorinMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B012MinBorinMiniProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hrd/borin/controller/MainController.java
@@ -0,0 +1,5 @@
+package com.hrd.borin.controller;
+
+public class MainController {
+
+}
