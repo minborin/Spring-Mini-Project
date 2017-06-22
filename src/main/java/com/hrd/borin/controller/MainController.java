b867381c3413c2a81cbe687e6131a4452772db80
Min Borin <minborin.gitlab@gmail.com>
2017-06-22 12:21:01 +0700

    add MainController

--- src/main/java/com/hrd/borin/controller/MainController.java
+++ src/main/java/com/hrd/borin/controller/MainController.java
@@ -1,5 +1,31 @@
 package com.hrd.borin.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
 
+	@RequestMapping("/user/user-cu")
+	public String userCU(){
+		return "/admin/user-cu";
+	}
+	
+	@RequestMapping("/user/user-list")
+	public String userList(){
+		return "/admin/user-list";
+	}
+	@RequestMapping("/role/role-c")
+	public String roleC(){
+		return "/admin/role-c";
+	}
+	@RequestMapping("/role/role-list")
+	public String roleList(){
+		return "/admin/role-list";
+	}
+	@RequestMapping("/user")
+	public String dashboard(){
+		return "/admin/dashboard";
+	}
 }
